4946bf590ffdcb818ad35ce248a588d889752c78
Hololens Dev Pc <bitter.daniel@gmail.com>
2020-02-16 09:21:02 -0600

    Init

---
+++ startServer.bat
@@ -0,0 +1,11 @@
+rem D:
+rem cd D:\Repos\PersonalGithub\AzureKinectKafka\k
+start cmd /k clearLogs.bat
+start cmd /k bin\windows\zookeeper-server-start.bat config/zookeeper.properties
+start cmd /k bin\windows\kafka-server-start.bat config/server.properties
+timeout /t 15 /nobreak > NUL
+rem bin\windows\kafka-topics.bat --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic testTopicName
+rem bin\windows\kafka-topics.bat --list --bootstrap-server localhost:9092
+start cmd /k bin\windows\kafka-console-producer.bat --broker-list localhost:9092 --topic testTopicName
+start cmd /k bin\windows\kafka-console-consumer.bat --bootstrap-server localhost:9092 --topic testTopicName --from-beginning
+exit
